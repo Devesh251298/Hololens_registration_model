68354bfa43cb20ce2308b3c738830471e7eee7ba
dj422 <dj422@ic.ac.uk>
2023-07-04 15:19:41 +0100

    Server Ready

--- src/deploy.py
+++ src/deploy.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import torch.utils.data
+import numpy as np
 from arguments import rpmnet_train_arguments
 from common.misc import prepare_logger
 from utils import get_model
@@ -30,12 +31,20 @@ def predict():
     data = request.json  # Get the input data from the request
     # Perform any necessary preprocessing on the input data
     # Convert the input data to a PyTorch tensor
-    input_tensor = torch.tensor(data["data_batch"])
+    input_tensor = data["data_batch"]
+    types = data["data_types"]
+    for key in input_tensor.keys():
+        if types[key] == "<class 'torch.Tensor'>":
+            input_tensor[key] = torch.tensor(input_tensor[key])
+        elif types[key] == "<class 'numpy.ndarray'>":
+            input_tensor[key] = np.array(input_tensor[key])
     # Run inference on the model
     with torch.no_grad():
-        output_tensor = model(input_tensor, args.num_reg_iter)
+        output_tensor, endpoints = model(input_tensor, args.num_reg_iter)
     # Convert the output tensor to a Python list
-    output = output_tensor.tolist()
+    output = output_tensor[0].cpu().detach().numpy()[0].tolist()
+    print(output)
+
     # Create a response dictionary with the model's predictions
     response = {"output": output}
     # Return the response as JSON
--- src/req.py
+++ src/req.py
@@ -3,6 +3,7 @@ import json
 import os
 import torch
 import torch.utils.data
+import numpy as np
 from arguments import rpmnet_train_arguments
 from common.misc import prepare_logger
 from data_loader.datasets import get_source, generate_data
@@ -22,9 +23,22 @@ else:
 
 source = get_source(args.object_file)
 data_batch = generate_data(source, args)
+data_types = {}
 
-data = {"data_batch": data_batch}
+for key in data_batch.keys():
+    # know the type of data_batch[key] and if it is a tensor or numpy array, convert it to a list 
+    # store the types first
+    data_types[key] = str(type(data_batch[key]))
+    if isinstance(data_batch[key], torch.Tensor):
+        data_batch[key] = data_batch[key].tolist()
+    elif isinstance(data_batch[key], np.ndarray):
+        data_batch[key] = data_batch[key].tolist()
+
+
+data = {"data_batch": data_batch, "data_types": data_types}
 json_data = json.dumps(data)
+
+print("Data sent to the server:")
 
 url = "http://localhost:5000/predict"  # Replace with your Flask route URL
 headers = {"Content-Type": "application/json"}
